fafb9022b6b1782e80467989141726b39ff83179
quackercrumbs <calvinquach@gmail.com>
2022-04-19 19:43:23 -0400

    downgrade to bevy 6 compat libs and include rapier
    downgrade because rapier 12 isn't ready for bevy 7
    https://github.com/dimforge/bevy_rapier/issues/134#issuecomment-1100622450

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use bevy::prelude::*;
 use bevy_flycam::PlayerPlugin;
+use bevy_rapier3d::prelude::*;
+
 fn main() {
    App::new()
        .add_plugins(DefaultPlugins)
        .add_plugin(PlayerPlugin)
+       .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
+       .add_plugin(RapierRenderPlugin)
        .add_startup_system(setup)
        .run();
 }
@@ -17,7 +21,7 @@ fn setup(
 ) {
    // plane
    commands.spawn_bundle(PbrBundle {
-       mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+       mesh: meshes.add(Mesh::from(bevy::prelude::shape::Plane { size: 5.0 })),
        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
        ..Default::default()
    });
