8e613a6cc4a2ba20624e62a4473e3a53adb8eb1c
quackercrumbs <calvinquach@gmail.com>
2022-04-19 18:41:39 -0400

    add bevy fly camera and plane to scene

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 use bevy::prelude::*;
-
+use bevy_flycam::PlayerPlugin;
 fn main() {
    App::new()
-       .add_system(hello_world_system)
+       .add_plugins(DefaultPlugins)
+       .add_plugin(PlayerPlugin)
+       .add_startup_system(setup)
        .run();
 }
 
-fn hello_world_system() {
-   println!("hello world");
+
+/// set up a simple 3D scene
+fn setup(
+   mut commands: Commands,
+   mut meshes: ResMut<Assets<Mesh>>,
+   mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+   // plane
+   commands.spawn_bundle(PbrBundle {
+       mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+       material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+       ..Default::default()
+   });
 }
