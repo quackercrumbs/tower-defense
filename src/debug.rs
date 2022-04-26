1adf0d52791c7c7feb0064c549fb3480de2c3dc4
quackercrumbs <calvinquach@gmail.com>
2022-04-26 10:26:08 -0400

    Add bevy inspector egui plugin for rapier

--- src/debug.rs
+++ src/debug.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use bevy_inspector_egui::WorldInspectorPlugin;
+use bevy_inspector_egui_rapier::InspectableRapierPlugin;
 
 pub struct DebugPlugin;
 
@@ -8,6 +9,7 @@ impl Plugin for DebugPlugin {
     fn build(&self, app: &mut App) {
         if cfg!(debug_assertions) {
             app.add_plugin(WorldInspectorPlugin::new());
+            app.add_plugin(InspectableRapierPlugin);
         }
     }
 
