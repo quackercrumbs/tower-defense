a989286ed439072189ef244995aa2e1b35e6bbd0
quackercrumbs <calvinquach@gmail.com>
2022-04-25 21:46:34 -0400

    fix: Cube collider and mesh to match size
    Note, the cube mesh internally divides by 2. while the rigid body
    internally treats them as halves already.

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,8 @@ fn main() {
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
         .add_plugin(RapierRenderPlugin)
         .add_startup_system(setup_world)
-        .add_system(print_ball_altitude)
+        .add_system(print_collider_altitude)
+        // .add_system(print_transform_altitude)
         .run();
 }
 
@@ -47,73 +48,60 @@ fn setup_world(
     });
 
     /* Create the ground. */
-    let ground_size = 200.1;
-    let ground_height = 0.1;
+    let ground_size = 20.1;
+    let ground_height = 0.5;
     commands
-        .spawn_bundle(ColliderBundle {
+        .spawn()
+        .insert_bundle(ColliderBundle {
             shape: ColliderShape::cuboid(ground_size, ground_height, ground_size).into(),
             position: Vec3::new(0.0, -ground_height, 0.0).into(),
             ..ColliderBundle::default()
         })
-        .insert_bundle(PbrBundle {
-            mesh: meshes.add(Mesh::from(bevy::prelude::shape::Plane {
-                size: ground_size,
-            })),
-            material: materials.add(Color::rgb(0.5, 0.5, 0.).into()),
-            ..Default::default()
-        })
+        // .insert_bundle(PbrBundle {
+        //     mesh: meshes.add(Mesh::from(bevy::prelude::shape::Cube {
+        //         size: ground_size,
+        //     })),
+        //     material: materials.add(Color::rgb(0.5, 0.5, 0.).into()),
+        //     ..Default::default()
+        // })
         .insert(ColliderDebugRender::default())
         .insert(ColliderPositionSync::Discrete);
 
-    /* Create the bouncing ball. */
-    commands
-        .spawn()
-        .insert_bundle(RigidBodyBundle {
-            position: Vec3::new(2.0, 2.0, 0.0).into(),
-            ..Default::default()
-        })
-        .insert_bundle(ColliderBundle {
-            shape: ColliderShape::ball(0.5).into(),
-            material: ColliderMaterial {
-                restitution: 0.7,
-                ..Default::default()
-            }
-            .into(),
-            ..Default::default()
-        })
-        .insert(ColliderPositionSync::Discrete)
-        .insert(ColliderDebugRender::default());
-
     // Create cube, Build the rigid body.
-    let rad = 1.0;
-    let color = 0;
+    let cube_size = 0.5;
     commands
         .spawn()
         .insert_bundle(RigidBodyBundle {
-            position: Vec3::new(1., 100., 0.).into(),
+            position: Vec3::new(1., 3., 0.).into(),
             ..RigidBodyBundle::default()
         })
         .insert_bundle(ColliderBundle {
-            shape: ColliderShape::cuboid(rad, rad, rad).into(),
+            shape: ColliderShape::cuboid(cube_size, cube_size, cube_size).into(),
             ..ColliderBundle::default()
         })
         // give it a mesh different from the collider shape
         .insert_bundle(PbrBundle {
-            mesh: meshes.add(Mesh::from(bevy::prelude::shape::Icosphere {
-                radius: 2.,
-                subdivisions: 10,
+            mesh: meshes.add(Mesh::from(bevy::prelude::shape::Cube {
+                size: cube_size * 2.,
             })),
             material: materials.add(Color::rgb(1., 0., 0.).into()),
-            transform: Transform::from_xyz(1., 1., 1.),
             ..Default::default()
         })
         // commented out debug mesh from bevy_rapier
-        .insert(ColliderDebugRender::with_id(color))
+        // .insert(ColliderDebugRender::with_id(color))
         .insert(ColliderPositionSync::Discrete);
 }
 
-fn print_ball_altitude(positions: Query<&RigidBodyPositionComponent>) {
+fn print_collider_altitude(positions: Query<&ColliderPositionComponent>) {
     for rb_pos in positions.iter() {
-        println!("Ball altitude: {}", rb_pos.position.translation.vector.y);
+        println!("Ball altitude: {}", rb_pos.translation.vector.y);
     }
+}
+
+fn print_transform_altitude(transforms: Query<&GlobalTransform>) {
+
+    for t in transforms.iter() {
+        println!("transform: {}", t.translation.y)
+    }
+
 }
